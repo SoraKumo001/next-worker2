ecacf38b24488abecc1ed9ed67eaca4ae1b7d60e
SoraKumo <info@croud.jp>
2021-08-21 22:20:03 +0900

    worker-libをnpmライブラリ化

--- src/libs/worker-test.ts
+++ src/libs/worker-test.ts
@@ -1,25 +1,31 @@
-import { createWorkerMap } from "./worker-lib";
+import { initWorker } from "worker-lib";
 
 const add = (a: number, b: number) => {
-  for (let i = 0; i < 1000000000; i++); //無駄に負荷をかける
+  for (let i = 0; i < 1000000000; i++); //Overload unnecessarily
   return a + b
 }
 const add2 = (a: string, b: string) => {
-  for (let i = 0; i < 1000000000; i++); //無駄に負荷をかける
+  for (let i = 0; i < 1000000000; i++); //Overload unnecessarily
   return a + b
 }
 const sub = (a: number, b: number) => {
-  for (let i = 0; i < 1000000000; i++); //無駄に負荷をかける
+  for (let i = 0; i < 1000000000; i++); //Overload unnecessarily
   return a - b
 }
 const mul = (a: number, b: number) => {
-  for (let i = 0; i < 1000000000; i++); //無駄に負荷をかける
+  for (let i = 0; i < 1000000000; i++); //Overload unnecessarily
   return a * b
 }
 
-// Workerで使用可能にするための初期化作業
-const map = createWorkerMap({ add, add2, sub, mul })
-// 型のみexportする
-export type WorkerMap = typeof map
+const error = (a: number, b: number) => {
+  for (let i = 0; i < 1000000000; i++); //Overload unnecessarily
+  throw new Error("throw")
+  return  a + b;
+}
+
+// Initialization process to make it usable in Worker.
+const map = initWorker({ add, add2, sub, mul, error })
+// Export only the type
+export type WorkerTest = typeof map
 
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import type { WorkerMap } from "../libs/worker-test";
-import { createWorker } from "../libs/worker-lib";
+import { createWorker } from "worker-lib";
+import type { WorkerTest } from "../libs/worker-test";
 
-// Worker実行用のインスタンスを作成
-// execute("関数名",...パラメータ)でWorkerが起動する
-const execute = createWorker<WorkerMap>(
+// Create an instance to execute the Worker
+// execute("function name",... parameter) to start the Worker
+const execute = createWorker<WorkerTest>(
   () => new Worker(new URL("../libs/worker-test", import.meta.url)),
-  5 //最大並列数
+  5 // Maximum parallel number
 );
 
 const Page = () => {
@@ -22,49 +22,61 @@ const Page = () => {
           type="button"
           onClick={async () => {
             const index = values.length;
-            setValues([...values, "実行中"]);
-            //Workerの呼び出し
-            const result = await execute("add", a, b); 
+            setValues([...values, "running"]);
+            //Calling a Worker
+            const result = await execute("add", a, b);
             setValues((values) => values.map((v, i) => (i === index ? result : v)));
           }}
         >
-          足し算
+          Add
         </button>
         <button
           type="button"
           onClick={async () => {
             const index = values.length;
-            setValues([...values, "実行中"]);
-            //Workerの呼び出し
+            setValues([...values, "running"]);
+            //Calling a Worker
             const result = await execute("add2", String(a), String(b));
             setValues((values) => values.map((v, i) => (i === index ? result : v)));
           }}
         >
-          足し算(文字列)
+          Add(String)
         </button>
         <button
           type="button"
           onClick={async () => {
             const index = values.length;
-            setValues([...values, "実行中"]);
-            //Workerの呼び出し
+            setValues([...values, "running"]);
+            //Calling a Worker
             const result = await execute("sub", a, b);
             setValues((values) => values.map((v, i) => (i === index ? result : v)));
           }}
         >
-          引き算
+          Sub
         </button>
         <button
           type="button"
           onClick={async () => {
             const index = values.length;
-            setValues([...values, "実行中"]);
-            //Workerの呼び出し
+            setValues([...values, "running"]);
+            //Calling a Worker
             const result = await execute("mul", a, b);
             setValues((values) => values.map((v, i) => (i === index ? result : v)));
           }}
         >
-          かけ算
+          Mul
+        </button>
+        <button
+          type="button"
+          onClick={async () => {
+            const index = values.length;
+            setValues([...values, "running"]);
+            //Calling a Worker
+            const result = await execute("error", a, b).catch((e) => e);
+            setValues((values) => values.map((v, i) => (i === index ? result : v)));
+          }}
+        >
+          Error
         </button>
       </form>
       {values.map((v, index) => (
@@ -73,5 +85,4 @@ const Page = () => {
     </div>
   );
 };
-
 export default Page;
